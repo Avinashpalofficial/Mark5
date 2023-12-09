895eb4a8d84808ccbd5e4471abf7f863e80d3b29
Avinashpalofficial <apal73451@gmail.com>
2023-12-09 12:03:24 +0530

    DAy 10

--- Untitled-1.cpp
+++ Untitled-1.cpp
@@ -1,18 +1,32 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-int indexOfKey(vector<int> &arr, int currIndex, int n, int key) {
-if(currIndex == n) {
-return -1;
+
+int findElementIndex(int arr[], int element, int index, int size) {
+    // Base case: If the entire array is traversed and element not found
+    if (index >= size) {
+        return -1;
+    }
+
+    // If the element is found, return its index
+    if (arr[index] == element) {
+        return index;
+    }
+
+    // Recursive case: Search in the rest of the array
+    return findElementIndex(arr, element, index + 1, size);
 }
-if(arr[currIndex] == key) {
-return currIndex;
-}
-return indexOfKey(arr, currIndex+1, n, key);
-}
+
 int main() {
-int n, key;
-cin >> n;
-vector<int> arr(n);
-for(int i = 0; i < n; ++i) {
-cin >> arr[i];
+    int arr[] = { 2, 4, 6, 8, 10 };
+    int element = 6;
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    int index = findElementIndex(arr, element, 0, size);
+    if (index != -1) {
+        cout << "Element found at index: " << index << endl;
+    } else {
+        cout << "Element not found in the array." << endl;
+    }
+
+    return 0;
 }
---
+++ sum_to_consecutive_Num.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+void print(int a[],int n){
+    //basecase
+    if(n<1) return ;
+     int temp[n-1];
+     for(int i=0;i<n-1;i++)temp[i] =a[i]+a[i+1]; 
+             
+
+     
+     for(auto x: temp)cout<<x<<endl;
+     print(temp,n-1);
+      
+}
+int main(){
+     int n;
+     cin>>n;
+     int a[n];
+     for(int i=0;i<n;i++){
+          cin>>a[n];
+     }
+      print(a,n);
+     //for(auto x: a) cout<<x<<endl;
+     
+    
+}
